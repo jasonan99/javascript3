09808bc7ecb54b2f4aa98ad899020c960adef3c1
jasonan99 <jasonan99@gmail.com>
2023-06-05 22:06:09 -0600

    add remove all function

--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,24 @@
 class Product {
-  constructor({ product, color, joke }) {
+  constructor({ product, color, joke,id }) {
     this.product = product;
     this.color = color;
     this.joke = joke;
+    this.id = id;
   }
 
   render() {
     const product = document.getElementById('product');
     product.innerHTML += `
-      <div class="product">
-        <img class="cart-img" src="../images/product-${this.product}-${this.color}.jpg"/>
-        <div>
-          <h3>${this.color} ${this.product} with joke</h3>
-          <p>Joke: ${this.joke}</p>
-          <button>Remove</button>
+      <div id="${this.id}" class="product">
+        <div class="cart-image">
+          <img class="cart-img" src="../images/product-${this.product}-${this.color}.jpg"/>
+        </div>  
+        <div class="cart-text">
+          <div>
+            <h3>${this.color} ${this.product} with joke</h3>
+            <p>Joke: ${this.joke}</p>
+            <button class="remove cart-btn">Remove</button>
+          </div>
         </div>
       </div>
     `;
--- src/components/cart.js
+++ src/components/cart.js
@@ -2,16 +2,20 @@ import { productChange } from './other-products.js';
 import { colorChange } from './colors.js';
 import { selected } from "../config.js";
 import Product from '../Product.js';
+import { selectedJoke } from './jokes-api.js';
 
 const add = document.getElementById('add-to-cart');
 const product = document.getElementById('product');
 const cart = document.getElementById('cart');
 const open = document.getElementById('cart-btn');
 const close = document.getElementById('close');
+const removeBtn = document.querySelectorAll('.remove');
 const removeAll = document.getElementById('remove-all');
+const joke = document.getElementById('main-joke');
 
 let state = selected;
 let products = [];
+let id = 0;
 
 function handleProductChange(product) {
   state.product = product;
@@ -27,9 +31,13 @@ function setUpSubscribers() {
 }
 
 function addToCart() {
-  const product = new Product({ product: state.product, color: state.color, joke: 'hola' });
-  products.push(product);
-  cart.appendChild(product.render());
+  product.style.display = "block";
+  id += 1;
+  let joker = joke.innerText;
+  console.log(joker)
+  const productC = new Product({ product: state.product, color: state.color, joke: `${joker}`, id: id });
+  products.push(productC);
+  cart.appendChild(productC.render());
   openCart();
 }
 
@@ -41,15 +49,29 @@ function closeCart() {
   cart.style.display = "none";
 }
 
+function remove() {
+  const selectedProduct = products.filter((element) => {
+    element.id == id;
+  })
+  console.log(selectedProduct);
+}
+
 function removeAllProducts() {
   products = [];
   product.style.display = "none";
 }
 
+// function getJoke() {
+//   const id = new URLSearchParams(window.location.search).get("id");
+//   let result = selectedJoke(id);
+//   return `${result}`;
+// };
+
 function initCart() {
   add.addEventListener('click', addToCart);
   close.addEventListener('click', closeCart);
   open.addEventListener('click', openCart);
+  // removeBtn.addEventListener('click', remove);
   removeAll.addEventListener('click', removeAllProducts);
   setUpSubscribers();
 }
